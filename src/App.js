5917c31b665097f85c917ad53910387342e4fe64
lemonade4813 <lemonade4813@gmail.com>
2022-10-16 16:46:19 +0900

    221016 프리온보딩 프론트엔드 코스 수정

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import './App.css';
 import {BrowserRouter as Router, Route,Routes, Navigate} from 'react-router-dom';
 import Todos from './Todos';
 import Login from './Login';
+import {useEffect, useState} from 'react'
 
 function App() {
+
+  // 로컬스토리지의 토큰값 상태값으로 설정
+
+  const [token, setToken] = useState("")
+
+  useEffect(()=>{
+      setToken(localStorage.getItem("token"))
+  }
+  ,[token])
+
 
   //로컬스토리지에 저장된 토큰값이 undefined으로 변경될경우 삭제
   if(localStorage.getItem('token') === "undefined"){
@@ -12,7 +23,7 @@ function App() {
   //로컬스토리지 토큰 저장 여부에 따른 페이지 분기
   return(
       <Router>
-      {  !localStorage.getItem('token') ? <Navigate to ="/" /> : <Navigate to="/todo" /> }
+      {  !token ? <Navigate to ="/" /> : <Navigate to="/todo" /> }
         <Routes>
           <Route path="/" element={<Login/>}></Route> 
           <Route path="/todo" element={<Todos/>}></Route>
